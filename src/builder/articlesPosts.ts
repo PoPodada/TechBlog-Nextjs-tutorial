70dbba24dbaa5feedf752ab62deddc5ab9d81d23
go <goito.722@icloud.com>
2024-03-29 14:03:45 +0900

    複数の著者から記事を取ることができた

--- src/builder/articlesPosts.ts
+++ src/builder/articlesPosts.ts
@@ -1,55 +1,63 @@
 import Parser from "rss-parser";
-import {JSDOM} from "jsdom";
+import { JSDOM } from "jsdom";
 
 const parser = new Parser();
 
 export type Member = {
-  feedLinks:string[]
-  name:string
-}
+  feedLinks: string[];
+  name: string;
+};
 export type Post = {
-  url:string
-  title:string
-  publishedAt:string
-  ogImageUrl:string
-  author:string
-}
+  url: string;
+  title: string;
+  publishedAt: string;
+  ogImageUrl: string;
+  author: string;
+};
 
 export const parseRSSFeed = async (url: string) => {
   const feed = await parser.parseURL(url);
 
-  return feed.items.map((item:any) => {
+  return feed.items.map((item: any) => {
     return {
-        title:item.title,
-        url:item.link,
-        publishedAt:item.pubDate
+      title: item.title,
+      url: item.link,
+      publishedAt: item.pubDate,
     };
   });
-  
 };
 
 export const getOgImagePath = async (url: string) => {
   const dom = await JSDOM.fromURL(url);
   const document = dom.window.document;
-  const documentMetadata = document.querySelector("meta[property='og:image']")?.getAttribute("content")
-  return documentMetadata; 
+  const documentMetadata = document
+    .querySelector("meta[property='og:image']")
+    ?.getAttribute("content");
+  return documentMetadata;
 };
 
-export const getArticlePost = async(member:Member):Promise<Post[]> => {
-  const member.feedLinks.map(async(feedUrl:string)=>{
-    const articles = await parseRSSFeed(feedUrl);
-    articles.map(async(article) => {
-      const ogImageUrl = await getOgImagePath(article.url);
-      return{
-          title: article.title,
-          url: article.url,
-          publishedAt: article.publishedAt,
-          ogImageUrl: ogImageUrl?ogImageUrl:"",
-          author:member.name
-      }
+export const getArticlePost = async (member: Member): Promise<Post[]> => {
+  const memberPostsEachByfeed = await Promise.all(
+    member.feedLinks.map(async (feedUrl: string) => {
+      const articles = await parseRSSFeed(feedUrl);
+      return await Promise.all(
+        articles.map(async (article) => {
+          const ogImageUrl = await getOgImagePath(article.url);
+          return {
+            title: article.title,
+            url: article.url,
+            publishedAt: article.publishedAt,
+            ogImageUrl: ogImageUrl ? ogImageUrl : "",
+            author: member.name,
+          };
+        })
+      );
     })
-  })
-  
-  
-  
-}
+  );
+  return memberPostsEachByfeed.flat(1);
+};
+
+export const getMembersPosts = async (members: Member[]): Promise<Post[]> => {
+  const memberPostsEachBymember = await Promise.all(members.map(getArticlePost))
+  return memberPostsEachBymember.flat(1);
+};
