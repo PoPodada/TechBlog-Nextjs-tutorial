9f75edf626c117ae426190170cc3267090f31e47
go <goito.722@icloud.com>
2024-03-29 21:17:23 +0900

    複数の記事サイトから取得することに成功した

---
+++ src/components/Markdown.tsx
@@ -0,0 +1,17 @@
+
+// import Markdown from 'react-markdown'
+// type Props ={text:string}
+// const Markdown = ({ text }:Props)=> {
+//     return(
+      
+//         <>
+//         <Markdown>
+//             {text}
+//         </Markdown>
+        
+//         </>
+        
+//       )
+// }
+
+// export default Markdown;
